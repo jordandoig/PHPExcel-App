4766c523b8d10b5bc61b014419e05e120abf1f82
Jordan Doig <jordandoig@live.com>
2017-09-05 14:50:15 -0600

    Added output filename as second argument

--- app.php
+++ app.php
@@ -2,11 +2,11 @@
   require_once "./vendor/phpoffice/phpexcel/Classes/PHPExcel.php";
 
   $fileName = $argv[1];
-  $excelReader = PHPExcel_IOFactory::createReaderForFile($fileName);
+  $excelReader = new PHPExcel_Reader_Excel2007();
   $excelObj = $excelReader->load($fileName);
   $worksheet = $excelObj->getSheet(0);
   $worksheet->setCellValue('C1', "Editted!");
 
   $objWriter = new PHPExcel_Writer_Excel2007($excelObj);
-  $objWriter->save(substr($fileName, 0, strlen($filename) - 5) . "_Editted.xlsx");
+  $objWriter->save($argv[2]);
 ?>
